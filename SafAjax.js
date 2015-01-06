b98e6d67094e21d899b49571d940c069ce4c7692
Scott Flaherty <Scott.Flaherty@mckesson.com>
2015-01-06 08:57:37 -0500

    rebuilt ajax class

--- SafAjax.js
+++ SafAjax.js
@@ -1,59 +1,129 @@
 // Safely create namespace
 var SAF = SAF || {};
 
-SAF.AjaxOptions = {
-    Url: null,
-    QueryString: null,
-    RequestType: null,
-    Success: null,
-    Error: null
-};
+/* EXAMPLE USAGE
 
-SAF.AjaxProvider = function () {
-    var options = null;
-
-    this.Call = function(ajaxOptions) {
-        options = ajaxOptions
-    };
-
-    var MakeRequest = function () {
-        var ids = ['MSXML2.XMLHTTP.3.0', 'MSXML2.XMLHTTP', 'Microsoft.XMLHTTP'], xhr;
-
-        if (window.XMLHttpRequest) {
-            xhr = new XMLHttpRequest();
-        } else {
-            for (var i = 0; i < ids.length; i++) {
-                try {
-                    xhr = new ActiveXObject(ids[i]);
-                    break;
-                } catch (e) {
-                }
-            }
+Ajax.request({
+        url: 'data.php',
+        method: 'post',
+        data: {
+            select: 'users',
+            orderBy: 'date'
+        },
+        headers: {
+            'custom-header': 'custom-value'
         }
+    })
+    .success(function(result) {
+        console.log("done", result);
+    })
+    .error(function(xhr) {
+        console.log("fail");
+    })
+    .always(function(xhr) {
+        console.log("always");
+    });
 
-        x.onreadystatechange = function () {
-            if (x.readyState == 4) {
-                if (x.status == 200) {
-                    options.Success(x.responseText);
+*/
+
+SAF.Ajax = {
+    request: function(options) {
+        if(typeof options == 'string') options = { url: options };
+
+        options.url = options.url || '';
+        options.method = options.method || 'get'
+        options.data = options.data || {};
+
+        var getParams = function(data, url) {
+            var arr = [], str;
+
+            for(var name in data) {
+                arr.push(name + '=' + encodeURIComponent(data[name]));
+            }
+
+            str = arr.join('&');
+
+            if(str != '') {
+                return url ? (url.indexOf('?') < 0 ? '?' + str : '&' + str) : str;
+            }
+
+            return '';
+        };
+
+        var api = {
+            host: {},
+            process: function(options) {
+                var self = this;
+                this.xhr = null;
+
+                if(window.ActiveXObject) {
+                    this.xhr = new ActiveXObject('Microsoft.XMLHTTP');
+                }
+                else if(window.XMLHttpRequest) {
+                    this.xhr = new XMLHttpRequest();
+                }
+
+                if(this.xhr) {
+                    this.xhr.onreadystatechange = function() {
+                        if(self.xhr.readyState == 4 && self.xhr.status == 200) {
+                            var result = self.xhr.responseText;
+
+                            if(options.json === true && typeof JSON != 'undefined') {
+                                result = JSON.parse(result);
+                            }
+
+                            self.successCallback && self.successCallback.apply(self.host, [result, self.xhr]);
+                        }
+                        else if(self.xhr.readyState == 4) {
+                            self.errorCallback && self.errorCallback.apply(self.host, [self.xhr]);
+                        }
+
+                        self.alwaysCallback && self.alwaysCallback.apply(self.host, [self.xhr]);
+                    }
+                }
+
+                if(options.method == 'get') {
+                    this.xhr.open("GET", options.url + getParams(options.data, options.url), true);
                 } else {
-                    options.Error(x);
+                    this.xhr.open(options.method, options.url, true);
+                    this.setHeaders({
+                        'X-Requested-With': 'XMLHttpRequest',
+                        'Content-type': 'application/x-www-form-urlencoded'
+                    });
+                }
+
+                if(options.headers && typeof options.headers == 'object') {
+                    this.setHeaders(options.headers);
+                }
+
+                setTimeout(function() {
+                    options.method == 'get' ? self.xhr.send() : self.xhr.send(getParams(options.data));
+                }, 20);
+
+                return this;
+            },
+            success: function(callback) {
+                this.successCallback = callback;
+                return this;
+            },
+            error: function(callback) {
+                this.errorCallback = callback;
+                return this;
+            },
+            always: function(callback) {
+                this.alwaysCallback = callback;
+                return this;
+            },
+            setHeaders: function(headers) {
+                for(var name in headers) {
+                    this.xhr && this.xhr.setRequestHeader(name, headers[name]);
                 }
             }
         };
 
-        xhr.open(options.RequestType, options.Url, true);
-
-        if (options.RequestType.toUpperCase() === 'GET') {
-            // When initiating a get request, the send function needs no arguments at all.
-            xhr.send();
-        } else if (options.RequestType.toUpperCase() === 'POST') {
-            xhr.send(options.QueryString);
-        }
-
-        // If you want to be extra careful, include an else here to handle a bad requestType
-    };
-};
-
+        return api.process(options);
+    }
+}
 
 
 
